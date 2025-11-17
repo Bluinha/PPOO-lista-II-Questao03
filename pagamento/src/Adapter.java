8765b3dcb86d4558e7fea9cf4fc911e43c367419
bluinha <cecyliahelenna@gmail.com>
2025-11-17 15:40:55 -0300

    Implemente os getters e setters do adapter que deve implementar o PPay.

--- pagamento/src/Adapter.java
+++ pagamento/src/Adapter.java
@@ -1,3 +1,50 @@
-public class Adapter {
-    // TODO: implementar adaptacao entre MPay e PPay
+public class Adapter implements PPay {
+
+    private MPay mpay;
+
+    public Adapter(MPay mpay) {
+        this.mpay = mpay;
+    }
+
+    public String getCustCardNo() {
+        return mpay.getCreditCardNo();
+    }
+
+    public String getCardOwnerName() {
+        return mpay.getCustomerName();
+    }
+
+    public String getCardExpMonthDate() {
+        return mpay.getCardExpMonth() + "/" + mpay.getCardExpYear();
+    }
+
+    public Integer getCVVNo() {
+        return mpay.getCardCVVNo().intValue();
+    }
+
+    public Double getTotalAmount() {
+        return mpay.getAmount();
+    }
+
+    public void setCustCardNo(String custCardNo) {
+        mpay.setCreditCardNo(custCardNo);
+    }
+
+    public void setCardOwnerName(String cardOwnerName) {
+        mpay.setCustomerName(cardOwnerName);
+    }
+
+    public void setCardExpMonthDate(String cardExpMonthDate) {
+        String[] p = cardExpMonthDate.split("/");
+        mpay.setCardExpMonth(p[0]);
+        mpay.setCardExpYear(p[1]);
+    }
+
+    public void setCVVNo(Integer cVVNo) {
+        mpay.setCardCVVNo(cVVNo.shortValue());
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        mpay.setAmount(totalAmount);
+    }
 }
