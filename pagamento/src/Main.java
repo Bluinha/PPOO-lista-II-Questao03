5785533d6f0d2cdf8069c75402094651484ad5a4
bluinha <cecyliahelenna@gmail.com>
2025-11-17 15:50:02 -0300

    Crie um Main que demonstre tudo o que foi implementado.

--- pagamento/src/Main.java
+++ pagamento/src/Main.java
@@ -9,7 +9,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        MpayImpl antigo = new MpayImpl();
+        antigo.setCustomerName("Maria Silva");
+        antigo.setCreditCardNo("5555-4444-3333-2222");
+        antigo.setCardExpMonth("06");
+        antigo.setCardExpYear("2030");
+        antigo.setCardCVVNo((short) 123);
+        antigo.setAmount(250.75);
 
-        
+        PPay adaptado = new Adapter(antigo);
+
+        System.out.println("=== Testando Pagamento com Adapter ===");
+        testPPay(adaptado);
     }
 }
